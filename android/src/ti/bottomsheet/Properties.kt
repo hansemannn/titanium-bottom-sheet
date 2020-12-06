05173b2770e85d5132f640d5f39a62cd872e6e94
Prashant Saini <>
2020-12-06 22:20:23 +0530

    feat: redesigned the entire module with scrolling support if content is large

---
+++ android/src/ti/bottomsheet/Properties.kt
@@ -0,0 +1,9 @@
+package ti.bottomsheet
+
+object Properties {
+    const val VIEW = "view"
+    const val CANCELABLE = "cancelable"
+    const val CANCELED_ON_TOUCH_OUTSIDE = "canceledOnTouchOutside"
+    const val BACKGROUND_COLOR = "backgroundColor"
+    const val PEEK_HEIGHT = "peekHeight"
+}
